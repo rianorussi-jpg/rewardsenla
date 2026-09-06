1c6aa16f89684475e59bd2cdc682712820e43f7a
rianorussi-jpg <rianorussi@gmail.com>
2026-09-05 21:50:18 -0600

    Update Supabase configuration values

--- app/assets/config.js
+++ app/assets/config.js
@@ -1,4 +1,4 @@
 window.ENLA_CONFIG = {
-  SUPABASE_URL: 'https://YOUR-PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY'
+  SUPABASE_URL: 'https://ywwffqvchwxrbfgesegc.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3d2ZmcXZjaHd4cmJmZ2VzZWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2NjA1ODcsImV4cCI6MjEwNDIzNjU4N30.c5TFPKN-qwVVO3V_mG5VeWhxHEmnsQelEPu3i11RIBc'
 };
